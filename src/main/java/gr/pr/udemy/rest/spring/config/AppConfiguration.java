fae84631a11e4e59c334405e26aefb16c84dee72
pressos <p.ressos@hotmail.com>
2019-03-06 10:36:43 +0200

    Fix dependencies and corrected mvc configurer

--- src/main/java/gr/pr/udemy/rest/spring/config/AppConfiguration.java
+++ src/main/java/gr/pr/udemy/rest/spring/config/AppConfiguration.java
@@ -3,10 +3,11 @@ package gr.pr.udemy.rest.spring.config;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan("gr.pr.udemy.rest.spring")
-public class AppConfiguration {
+public class AppConfiguration implements WebMvcConfigurer {
 
 }
